6f54d0a8310d1f43aebe901279a2f4d9f287a536
Hayden Rahn <anhayzzing33@gmail.com>
2018-08-01 00:58:42 -0500

    login logout

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-
+import {AuthService} from "../../services/auth.service"
+import {Router} from "@angular/router"
+import {FlashMessagesService} from "angular2-flash-messages"
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -8,8 +10,25 @@ import { Component, OnInit } from "@angular/core";
 export class LoginComponent implements OnInit {
   username: String;
   password: String;
-  constructor() {}
+  constructor(
+    private aService : AuthService,
+    private router : Router,
+    private msg : FlashMessagesService,
+  ) {}
 
   ngOnInit() {}
-  onLoginSubmit() {}
+  onLoginSubmit() {
+    const user ={
+      username : this.username,
+      password : this.password
+    }
+    this.aService.authenticateUser(user).subscribe(data=>{
+     if(data.success){
+      this.aService.storeUserData(data.token,data.user)
+      this.msg.show('You are now logged in',{cssClass:'alert-success',timeout:3000})
+      this.router.navigate(['dashboard'])
+     }
+     else{this.msg.show(data.msg,{cssClass:'alert-danger',timeout:3000})}
+    })
+  }
 }
